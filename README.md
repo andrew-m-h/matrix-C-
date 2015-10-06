2f6f5e62b5651d318a99bb66b99cff7115e4c6d6
Andrew Morgan Hall <u5825803@anu.edu.au>
2015-10-07 00:11:03 +1100

    Serious re-write of main method to incorperate all the nessecairy saftey checks

---
+++ README.md
@@ -0,0 +1,54 @@
+# Invert Matrix
+
+### Introduction
+Invert matrix is a command line tool, allowing one to easily invert an n*n matrix of numbers (int/float/double/long...).
+The tool is built, using my personal, though not private, Matrix library originally written for Visual C++, but converted to unix C++ to conform with the C++11 standard.
+
+### Usage
+The tool reads matricies from a file, and can output an inverted matrix to either a file, or the terminal.
+
+    $ invert-matrix -d Dimension -i Input [-o Output] [-p Precision] [-h Help]
+        Dimension: The dimension of the matrix, greater than 1
+        Input: Input file name of file containing input matrix
+        Precision: Desired precision of output matrix
+        Help: This help message
+
+### Input File
+The input file represents a stream of numbers, which will be read, left to right, top to bottom into the matrix of given dimension (remembering that only square matricies are invertable). This means that the input file can be a list of space seperated numbers, tab seperated with newlines or any mixture.
+
+e.g. space seperated\
+1	8	-9	7	5
+0	1	0	4	4
+0	0	1	2	5
+0	0	0	1	-5
+0	0	0	0	1
+
+e.g. space seperated with new line characters\
+1	8	-9	7	5\
+0	1	0	4	4\
+0	0	1	2	5\
+0	0	0	1	-5\
+0	0	0	0	1
+
+### Limitations
+The tool uses the method of cofactor expansion to invert a matrix, this gives exponential time and limits the size of the input matrix to realistically less than 10x10. Internally, the numbers are represenetd as double precision, this leads to the all too common limitations when working with high precisions.
+
+### Installation
+There are two ways to compile the project, both use the g++ compiler. There is included a Makefile with the project, a simple call to 
+
+    $ make
+ should be enough to compile the project.\
+ However, for those who wish to edit the source code, a codebocks file is included as well, and can be used to compile, debug and edit the project.\
+To test the projecct, a test 5x5 matrix is provied in the file "matrix.txt". Run:
+ 
+    $ ./invert-matrix -d 5 -i matrix.txt
+    $ ./invert-matrix -d 5 -i matrix.txt -o matrix-output.txt
+Remember, if your using code::blocks, copy matrix.txt into the directory of the binary.\
+The appropriate output from these tests should be printeed to the terminal and written to the file matrix-output.txt:
+
+1	-8	9	7	17\
+-0	1	-0	-4	-24	\
+0	-0	1	-2	-15\
+-0	0	-0	1	5\
+0	-0	0	-0	1	
+
--- main.cpp
+++ main.cpp
@@ -4,46 +4,128 @@
 #include <iterator>
 #include <algorithm>
 #include <sstream>
+#include <map>
+#include <string>
 #include "matrix.h"
+
+enum Argument{
+    Dimension = 0,
+    Input,
+    Output,
+    Precision
+};
+
+const char* helpMessage = "usage:\n\tinvert-matrix -d Dimension -i Input [-o Output] [-p Precision] [-h Help]\n\tDimension: The dimension of the matrix, greater than 1\n\tInput: Input file name of file containing input matrix\n\tPrecision: Desired precision of output matrix\n\tHelp: This help message";
+const int defaultPrecision = 3;
+
+bool setPrec(std::ostream &out, const std::string &str);
 
 int main(int argc, char *argv[])
 {
-    if (argc == 1){
-        std::cout << "usage:\n\t invert-matrix height filename [-o output file]\n\t to invert a space seperated matrix\n\t invert-matrix" << std::endl;
-        return 0;
-    } else {
-        int length;
-
-        std::istringstream ass(argv[1]);
-        ass >> length;
-
-        std::ifstream matrix_file (argv[2]);
-        if( !matrix_file.is_open() ){
-            std::cout << "could not open file: " << argv[2] << std::endl;
-            return 0;
-        } else {
-            std::vector<double> data;
-            std::copy(
-                std::istream_iterator<double>(matrix_file),
-                std::istream_iterator<double>(),
-                std::back_inserter(data)
-            );
-            try {
-                Matrix::matrix<double> A(length, length, &data);
-                if (argc == 3) {
-                    std::cout.precision(3);
-                    std::cout << Matrix::invert(A);
-                } else if (argc == 5 && argv[3][1] == 'o') {
-                    std::ofstream output (argv[4]);
-                    output.precision(3);
-                    output << Matrix::invert(A);
-                    output.close();
-                }
-                matrix_file.close();
-            } catch (Matrix::matrixException e){
-                std::cerr << e.getErrorMessage() << std::endl;
+    //Place all command line arguments into argument map using Argument Enum as key
+    std::map<Argument, std::string> arguments;
+    for (int i = 1; i < argc; i+=2){
+        switch(argv[i][1]){
+            case 'd' : arguments[Dimension] = argv[i+1];break;
+            case 'i' : arguments[Input] = argv[i+1];break;
+            case 'o' : arguments[Output] = argv[i+1];break;
+            case 'p' : arguments[Precision] = argv[i+1];break;
+            case 'h' :
+                std::cout << helpMessage << std::endl;
                 return 0;
-            }
+            default  : std::cout << argv[i][1] << " is not a valid command line argument"; return 0;
         }
     }
+    //The mandatory arguments are Dimension and Input, if they are not present, then warn the user to user and exit.
+    if ( arguments.find(Input) == arguments.end() || arguments.find(Dimension) == arguments.end() ){
+        std::cout << "Must have -i and -d arguments\n" << helpMessage << std::endl;
+        return 0;
+    } else {
+        //Hold the dimension of the matrix in question
+        int dim;
+        //casting command line argument to integer requires much checking
+        try{
+            dim = std::stoi(arguments[Dimension]);
+            if (dim <= 1){
+                std::cout << "The dimension must be greater than 1" << std::endl;
+                return 0;
+            }
+        } catch (std::invalid_argument e){
+            std::cout << arguments[Dimension] << " is not a valid dimension, dimension must be an integer" << std::endl;
+            return 0;
+        }
+
+        //Open input file
+        std::ifstream matrix_file(arguments[Input]);
+        if( !matrix_file.is_open() ){
+            std::cout << "could not open file: " << argv[Input] << std::endl;
+            return 0;
+        }
+        //Read file contents into data vector
+        std::vector<double> data;
+        std::copy(
+            std::istream_iterator<double>(matrix_file),
+            std::istream_iterator<double>(),
+            std::back_inserter(data)
+        );
+        matrix_file.close();
+
+        try {
+            Matrix::matrix<double> A(dim, dim, &data);
+            //If the output is to go to an output file
+            if (arguments.find(Output) == arguments.end()){
+                //Set precision
+                if (arguments.find(Precision) == arguments.end()){
+                    std::cout.precision(defaultPrecision);
+                } else {
+                    if (!setPrec(std::cout, arguments[Precision]))
+                        return 0;
+                }
+                //INVERT!
+                std::cout << Matrix::invert(A);
+            } else {
+                //Open/Create output file
+                std::ofstream output(arguments[Output]);
+                if (!output.is_open()){
+                    std::cout << "Could not open file: " << argv[Output] << std::endl;
+                    return 0;
+                }
+                //Set precision
+                if (arguments.find(Precision) == arguments.end()){
+                    output.precision(defaultPrecision);
+                } else {
+                    if (!setPrec(output, arguments[Precision]))
+                        return 0;
+                }
+                //INVERT!
+                output << Matrix::invert(A);
+                output.close();
+            }
+        } catch (Matrix::matrixException e){
+            std::cerr << e.getErrorMessage() << std::endl;
+            return 0;
+        }
+    }
+    return 0;
 }
+
+/*
+Set the precision of an output stream based on the command line argument
+This saves having to do all the checking twice
+return true is all is successful, return false if operaion fails
+*/
+bool setPrec(std::ostream &out, const std::string &str){
+    try {
+        int p = std::stoi(str);
+        if (p < 0 || p > 12) {
+            std::cout << str << " is not a valid precision\n precision must be greater than or equal to 0 and less than or equal to 12" << std::endl;
+            return false;
+        }
+        out.precision(p);
+    } catch (std::invalid_argument e) {
+        std::cout << str << " is not a valid precision, precision must be an integer" << std::endl;
+        return false;
+    }
+    return true;
+}
+
